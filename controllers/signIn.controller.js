29d7a3e4b96cd7813bb482d44885d64247c0a111
Callmedoziee <francisnwosu017@gmail.com>
2022-06-27 11:58:45 +0100

    format

--- controllers/signIn.controller.js
+++ controllers/signIn.controller.js
@@ -1,38 +1,39 @@
 const student = require("../models/student.model");
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-async function studentSignIn(req,res){
-    const {email, password} = req.body;
-    try {
-        const currentStudent = await student.findOne({email});
+async function studentSignIn(req, res) {
+  const { email, password } = req.body;
+  try {
+    const currentStudent = await student.findOne({ email });
 
-        if(currentStudent == null || typeof currentStudent == undefined) return res.status(401).json({error : "User does not exist"})
-    
-        const passwordCompare = await bcrypt.compare(password,currentStudent.password);
-        if(passwordCompare){
-            const token = jwt.sign({email},process.env.TOKEN_KEY)
-            const response = {
-                currentStudent,
-                authToken : token
-            }
-            return res.status(200).json(response)
-        }else{
-            return res.status(401).json("Incorrect Password")
-        }        
-    } catch (error) {
-        console.log(error.message);
+    if (currentStudent == null || typeof currentStudent == undefined)
+      return res.status(401).json({ error: "User does not exist" });
+
+    const passwordCompare = await bcrypt.compare(
+      password,
+      currentStudent.password
+    );
+    if (passwordCompare) {
+      const token = jwt.sign({ email }, process.env.TOKEN_KEY);
+      const response = {
+        currentStudent,
+        authToken: token,
+      };
+      return res.status(200).json(response);
+    } else {
+      return res.status(401).json("Incorrect Password");
     }
-    
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-async function companySignIn(req,res){
-
-    return 0;
+async function companySignIn(req, res) {
+  return 0;
 }
-
 
 module.exports = {
-    studentSignIn,
-    companySignIn
-}
+  studentSignIn,
+  companySignIn,
+};
