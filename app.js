6cf7c48bc6d0c34a0ea1489376dfea31c8c196ae
Callmedoziee <francisnwosu017@gmail.com>
2022-06-27 10:51:14 +0100

    ends

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 require("dotenv").config();
 
-const express =  require("express");
+const express = require("express");
 const dbConnect = require("./middlewares/dbUtil/dbconnect");
 const app = express();
-const studentRegister = require("./models/student.model")
-app.get("/", async (req, res)=>{
- const student =  await studentRegister.create({
-        firstname :"chiedozie",
-        lastname:"nwosu",
-        email:"dozie.come",
-        password:"doings",
-        fieldOfInterest:["Medicine", "Kung fu", "Programming", "Money ways"],
-        graduation:[ 
-            {    
-                status:"somehow",
-                schoolName:"somehow",
-                startYear:"somehow",
-                degree:"somehow",
-                gpa:"E dey",
-                gpaScale:"E dey"
-            }]
-        })
-console.log("saved" + student);
-    return res.send("him");
-})
+const studentRegister = require("./models/student.model");
+app.get("/", async (req, res) => {
+  const student = await studentRegister.create({
+    firstname: "chiedozie",
+    lastname: "nwosu",
+    email: "dozie.come",
+    password: "doings",
+    fieldOfInterest: ["Medicine", "Kung fu", "Programming", "Money ways"],
+    graduation: [
+      {
+        status: "somehow",
+        schoolName: "somehow",
+        startYear: "somehow",
+        degree: "somehow",
+        gpa: "E dey",
+        gpaScale: "E dey",
+      },
+    ],
+  });
+  console.log("saved" + student);
+  return res.send("him");
+});
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 const SERVER = "localhost";
-dbConnect().then(()=>{
-    app.listen(PORT, SERVER, ()=>{
-        console.log(`Server running on http://${SERVER}:${PORT}`);
-    })
-})
-
+dbConnect().then(() => {
+  app.listen(PORT, SERVER, () => {
+    console.log(`Server running on http://${SERVER}:${PORT}`);
+  });
+});
