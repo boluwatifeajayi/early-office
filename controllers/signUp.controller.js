d9f48c539d53902551805d342141d91a9a334cc0
Callmedoziee <francisnwosu017@gmail.com>
2022-06-29 22:08:57 +0100

    oauth cont'd

--- controllers/signUp.controller.js
+++ controllers/signUp.controller.js
@@ -1,93 +1,125 @@
 const companyModel = require("../models/company.model");
 const studentModel = require("../models/student.model");
-const hashPassword = require("../middlewares/helperfunctions/hashPassword.helper").hashPassword;
-const jwt = require("jsonwebtoken")
+const axios = require("axios");
+const hashPassword =
+  require("../middlewares/helperfunctions/hashPassword.helper").hashPassword;
+const jwt = require("jsonwebtoken");
 
-async function studentSignUp (req, res){
-    // destructuring req.body
-    const {email, lastname, firstname, password , phoneNumber} = req.body;
-    const hashedPassword = await hashPassword(password);
-    try {
-        const newStudent = await studentModel.create({
-            email, 
-            lastname,
-            firstname, 
-            password : hashedPassword,
-            phoneNumber
-        })
-        console.log("created new student @" + newStudent);
+async function studentSignUp(req, res) {
+  // destructuring req.body
+  const { email, lastname, firstname, password, phoneNumber } = req.body;
+  const hashedPassword = await hashPassword(password);
+  try {
+    const newStudent = await studentModel.create({
+      email,
+      lastname,
+      firstname,
+      password: hashedPassword,
+      phoneNumber,
+    });
+    console.log("created new student @" + newStudent);
 
-        const token = jwt.sign(
-            { id: newStudent._id.toString(), email },
-            process.env.TOKEN_KEY
-        );
+    const token = jwt.sign(
+      { id: newStudent._id.toString(), email },
+      process.env.TOKEN_KEY
+    );
 
-        const response = {
-            newStudent,
-            authToken: token,
-        };
+    const response = {
+      newStudent,
+      authToken: token,
+    };
 
-        res.cookie("authToken",token)
-        return res.status(201).json(response);
-    } catch (error) {
-        console.log(error.message);
+    res.cookie("authToken", token);
+    return res.status(201).json(response);
+  } catch (error) {
+    console.log(error.message);
 
-        if(error.code == 11000)  return res.status(400).json({error: "Provide a valid email"});
-        return res.status(400).json({error:error.message});
-    }
+    if (error.code == 11000)
+      return res.status(400).json({ error: "Provide a valid email" });
+    return res.status(400).json({ error: error.message });
+  }
 }
 
+async function companySignUp(req, res) {
+  // destructuring req.body
+  const {
+    AdminFirstName,
+    AdminLastName,
+    orgEmail,
+    orgPassword,
+    phoneNumber,
+    orgName,
+    orgDescription,
+  } = req.body;
+  console.log(req.body);
+  const hashedPassword = await hashPassword(orgPassword);
+  try {
+    const newCompany = await companyModel.create({
+      AdminFirstName,
+      AdminLastName,
+      orgEmail,
+      orgPassword: hashedPassword,
+      phoneNumber,
+      orgName,
+      orgDescription,
+    });
+    console.log("created new company @" + newCompany);
 
+    const token = jwt.sign(
+      { id: newCompany._id.toString(), orgEmail },
+      process.env.TOKEN_KEY
+    );
+    const response = {
+      newCompany,
+      authToken: token,
+    };
 
-
-
-async function companySignUp (req, res){
-    // destructuring req.body
-    const {AdminFirstName, AdminLastName, orgEmail, orgPassword, phoneNumber, orgName, orgDescription} = req.body;
-    console.log(req.body)
-    const hashedPassword = await hashPassword(orgPassword);
-    try {
-        const newCompany = await companyModel.create({
-            AdminFirstName,
-            AdminLastName,
-            orgEmail,
-            orgPassword : hashedPassword,
-            phoneNumber,
-            orgName,
-            orgDescription
-        });
-        console.log("created new company @" + newCompany);
-
-        const token = jwt.sign(
-            { id: newCompany._id.toString(), orgEmail },
-            process.env.TOKEN_KEY
-          );
-          const response = {
-            newCompany,
-            authToken: token,
-          };
-        
-          res.cookie("authToken",token)
-        return res.status(201).json(response);
-    } catch (error) {
-        console.log(error.message);
-        if(error.code == 11000)  return res.status(400).json({error: "Account already exists"});
-        return res.status(400).json({error:error.message});
-    }
-
+    res.cookie("authToken", token);
+    return res.status(201).json(response);
+  } catch (error) {
+    console.log(error.message);
+    if (error.code == 11000)
+      return res.status(400).json({ error: "Account already exists" });
+    return res.status(400).json({ error: error.message });
+  }
 }
 
+async function oauthSignup(req, res) {
+  try {
+    console.log("NEW REQUEST!!!");
+    const CLIENT_ID =
+      "906540842423-7ki40si5b62f8dvlem89emrm28vk83rm.apps.googleusercontent.com";
+    const CLIENT_SECRET = "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";
+    const REDIRECT_URI = "http://localhost:4500/api/oauth/google/signup";
+    const SIGN_WITH_GOOGLE = `https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/userinfo.profile&include_granted_scopes=true&response_type=code&state=state&redirect_uri=${REDIRECT_URI}&client_id=${CLIENT_ID}`;
+    const { code } = req.query;
+    const response = await axios.post(
+      `https://www.googleapis.com/oauth2/v4/token?code=${code}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&redirect_uri=${REDIRECT_URI}&grant_type=authorization_code`
+    );
+    const { access_token } = response.data;
+    console.log("access token =  " + access_token);
+    const getUserData = await axios.get(
+      `https://www.googleapis.com/oauth2/v2/userinfo?access_token=${access_token}`
+    );
+    console.log("user data: " + { ...getUserData });
+    const {
+      email,
+      given_name: firstname,
+      family_name: lastname,
+    } = getUserData.data;
 
-async function oauthSignup (req,res) {
-    try{
-
-    }catch(error){
-
-    }
+    res.json({ email, firstname, lastname });
+    // NOW WE CAN TOKENIZE THE CURRENT DATA WE HAVE USING THE EMAIL AND ...
+    // NOTE: IDK HOW WE'D SET PASSWORD FOR THE USER
+    // const token = jwt.sign({ id: newStudent._id.toString(), email}, process.env.TOKEN_KEY);
+    //    const response = {newStudent ,authToken: token};
+  } catch (error) {
+    res.send(error.message);
+  }
 }
 
 module.exports = {
-    studentSignUp,
-    companySignUp,
-    oauthSignup
-}
+  studentSignUp,
+  companySignUp,
+  oauthSignup,
+};
--- routes/signUp.route.js
+++ routes/signUp.route.js
@@ -2,20 +2,29 @@ const express = require("express");
 const { oauthSignup } = require("../controllers/signUp.controller");
 const validation = require("../middlewares/validation/validation");
 
-const { studentSignUpSchema, companySignUpSchema } = require("../middlewares/validation/validation.Schema/signUp.schema");
+const {
+  studentSignUpSchema,
+  companySignUpSchema,
+} = require("../middlewares/validation/validation.Schema/signUp.schema");
 const studentSignUp = require("../controllers/signUp.controller").studentSignUp;
 const companySignUp = require("../controllers/signUp.controller").companySignUp;
 var route = express.Router();
 
 // Student signup route
-route.post("/api/student/signUp", validation(studentSignUpSchema), studentSignUp)
+route.post(
+  "/api/student/signUp",
+  validation(studentSignUpSchema),
+  studentSignUp
+);
 
 // Student oauth route
-route.post("/api/oauth/google/signUp", oauthSignup)
-
-
+route.get("/api/oauth/google/signup", oauthSignup);
 
 // Company signup route
-route.post("/api/company/signUp", validation(companySignUpSchema), companySignUp)
+route.post(
+  "/api/company/signUp",
+  validation(companySignUpSchema),
+  companySignUp
+);
 
-module.exports = route
+module.exports = route;
