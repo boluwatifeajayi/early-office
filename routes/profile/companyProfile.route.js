f1973a4463ee34296f3423f04cad8ba3fb6c9281
Ekundee <85397228+Ekundee@users.noreply.github.com>
2022-06-30 21:08:40 +0100

    Company profile

--- routes/profile/companyProfile.route.js
+++ routes/profile/companyProfile.route.js
@@ -1,4 +1,14 @@
-const express = require("express")
+const express = require("express");
+const { updateCompanyProfile } = require("../../controllers/profile/companyProfile.controller");
+const { changeStudentPassword } = require("../../controllers/profile/studentProfile.controller");
+const { protectedRoutes } = require("../../middlewares/authentication/protectedRoutes");
 var route = express.Router();
+
+// Update Company profile 
+route.post("/api/Company/profile/update",protectedRoutes, updateCompanyProfile)
+
+// Change Company password
+route.post("/api/Company/change/password",protectedRoutes,changeStudentPassword)
+
 
 module.exports = route;
